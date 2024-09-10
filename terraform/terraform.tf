044b40ac14e87f0165fe40e6f7c483f68fdf607f
marcuskoh95 <marcuskoh95@gmail.com>
2024-09-10 16:23:02 +0800

    edited cluster, vpc names and region code

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "marcuskoh95-gitops-proj-s3-terraformstate"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"

fb5a69b4f30c5a56e6ca3f1764e611fa9794d9f1
marcuskoh95 <marcuskoh95@gmail.com>
2024-10-08 20:14:48 +0800

    updated eks-cluster.tf to include correct eks node group permissions

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -11,7 +11,6 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
@@ -24,7 +23,6 @@ module "eks" {
       max_size     = 3
       desired_size = 3
     }
-
   }
 }
 
@@ -36,10 +34,44 @@ resource "aws_security_group_rule" "allow_http" {
   protocol                 = "tcp"
   source_security_group_id = "sg-0d38232bdeda92bc9"
 
-  /*lifecycle {
-    prevent_destroy = true
-  }*/
-
-  # Reference the EKS cluster security group from the module output
   security_group_id = module.eks.cluster_security_group_id
+}
+
+# Attach specific permissions to the EKS node group IAM role
+resource "aws_iam_policy" "node_group_permissions" {
+  name        = "NodeGroupPermissions"
+  description = "Permissions for EKS node group to manage auto-scaling and EKS actions"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Statement1"
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:UpdateAutoScalingGroup",
+          "autoscaling:AttachInstances",
+          "autoscaling:DetachInstances",
+          "autoscaling:SetDesiredCapacity",
+          "ec2:DescribeLaunchTemplateVersions",
+          "eks:DescribeNodegroup",
+          "eks:ListNodegroups"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the specific permissions policy to the EKS node group IAM role
+resource "aws_iam_role_policy_attachment" "node_group_permissions_attachment" {
+  role       = module.eks.node_groups["one"].iam_role_arn
+  policy_arn = aws_iam_policy.node_group_permissions.arn
+}
+
+# Attach the AmazonEKSClusterPolicy to the EKS node group IAM role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = module.eks.node_groups["one"].iam_role_arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
